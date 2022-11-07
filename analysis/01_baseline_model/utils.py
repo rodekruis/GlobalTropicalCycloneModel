6ed1b5a9528ea4fdf90c07ce559419a31d8dd646
Monica Turner <turnermon@gmail.com>
2022-11-07 14:48:45 +0100

    Directory restructure (#15)

--- analysis/01_baseline_model/utils.py
+++ analysis/01_baseline_model/utils.py
@@ -4,8 +4,11 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 
+
 def get_clean_dataset():
-    input_dir = Path(os.getenv("STORM_DATA_DIR")) / "input/01_baseline_model"
+    input_dir = (
+        Path(os.getenv("STORM_DATA_DIR")) / "analysis/01_baseline_model/input"
+    )
 
     # Read in input data
     combined_input_data = pd.read_csv(input_dir / "combined_input_data.csv")
@@ -23,7 +26,9 @@ def get_clean_dataset():
             value = damage
         elif v_max > x_max or rainfall_max > y_max:
             value = damage
-        elif v_max < np.sqrt((1 - (rainfall_max**2 / y_max**2)) * x_max**2):
+        elif v_max < np.sqrt(
+            (1 - (rainfall_max ** 2 / y_max ** 2)) * x_max ** 2
+        ):
             value = 0
         # elif ((v_max < x_max)  and  (rainfall_max_6h < y_max) ):
         # elif (v_max < x_max ):
@@ -39,12 +44,14 @@ def get_clean_dataset():
 
     # TODO: I thought we want to keep NA damage values
     # Remove NA values
-    combined_input_data = combined_input_data[combined_input_data["DAM_perc_dmg"].notnull()]
+    combined_input_data = combined_input_data[
+        combined_input_data["DAM_perc_dmg"].notnull()
+    ]
 
     # Create cubed wind feature
-    combined_input_data["HAZ_v_max_3"] = combined_input_data["HAZ_v_max"].apply(
-        lambda x: x * x * x
-    )
+    combined_input_data["HAZ_v_max_3"] = combined_input_data[
+        "HAZ_v_max"
+    ].apply(lambda x: x * x * x)
 
     # Drop Mun_Code since it's not a feature
     combined_input_data = combined_input_data.drop(columns="Mun_Code")
